64526718cf221b4d03d98d2c1ba419a27f7b0797
jax <jax_builder@gamil.com>
2024-03-17 19:45:08 +0800

    fix install load model

--- install.py
+++ install.py
@@ -54,12 +54,12 @@ print('downloading wav2lip essentials')
 load_file_from_url(
   url='https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip_GAN.pth',
   model_dir='checkpoints', progress=True, file_name='Wav2Lip_GAN.pth')
-model = load_model(os.path.join(working_directory,'checkpoints','Wav2Lip_GAN.pth'))
+# model = load_model(os.path.join(working_directory,'checkpoints','Wav2Lip_GAN.pth'))
 print('wav2lip_gan loaded')
 load_file_from_url(
   url='https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/Wav2Lip.pth',
   model_dir='checkpoints', progress=True, file_name='Wav2Lip.pth')
-model = load_model(os.path.join(working_directory,'checkpoints','Wav2Lip.pth'))
+# model = load_model(os.path.join(working_directory,'checkpoints','Wav2Lip.pth'))
 print('wav2lip loaded')
 
 #download gfpgan files
@@ -67,7 +67,7 @@ print("downloading gfpgan essentials")
 load_file_from_url(
   url='https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/GFPGANv1.4.pth',
   model_dir='checkpoints', progress=True, file_name='GFPGANv1.4.pth')
-load_sr()
+# load_sr()
 
 #load face detectors
 print('initializing face detectors')
@@ -75,7 +75,7 @@ load_file_from_url(
   url='https://github.com/anothermartz/Easy-Wav2Lip/releases/download/Prerequesits/shape_predictor_68_face_landmarks_GTX.dat',
   model_dir='checkpoints', progress=True, file_name='shape_predictor_68_face_landmarks_GTX.dat')
 
-load_predictor()
+# load_predictor()
 
 #write a file to signify setup is done
 with open('installed.txt', 'w') as f:
